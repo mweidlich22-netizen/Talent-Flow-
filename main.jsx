a4ab1de035499d40db62ac220b385a29706a987c
mweidlich22-netizen <mweidlich22@gmail.com>
2025-12-02 13:48:26 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TalentflowLanding from './TalentflowLanding.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TalentflowLanding />
+  </React.StrictMode>,
+)
